23cd9f194587425255ef4d8ca2867bf4a3387f7f
GUjiYN <2946459904@qq.com>
2024-03-22 21:23:13 +0800

    修改密码对接

--- src/views/UserPage/UserPassword.vue
+++ src/views/UserPage/UserPassword.vue
@@ -2,58 +2,63 @@
   <div class="container w-full">
     <div class="grid gap-3">
       <div class="grid grid-cols-12 gap-3">
-        <div class="col-span-12 text-xl"><ToolTwoTone /> 修改密码</div>
-        <div class="col-span-12">
-            <div class="container p-3">
-              <div class="row-auto">
-                <div class="row-span-1">
-                  <a-form
-                      :label-col="{ span: 7 }"
-                      :model="data"
+        <div class="col-span-12 text-xl">
+          <ToolTwoTone/>
+          修改密码
+        </div>
+        <div class="col-span-12 px-3 md:px-10 lg:px-20 xl:px-40">
+          <div class="container p-3">
+            <div class="row-auto">
+              <div class="row-span-1">
+                <a-form
+                    :label-col="{ span: 4 }"
+                    :model="data"
+                >
+                  <a-form-item
+                      :rules="[{ required: true, message: '请输入当前密码' }]"
+                      label="当前密码"
+                      name="currentPassword"
                   >
-                    <a-form-item
-                        :rules="[{ required: true, message: '请输入当前密码' }]"
-                        label="当前密码"
-                        name="currentPassword"
-                    >
-                      <a-input v-model:value="data.currentPassword">
-                        <template #prefix>
-                          <MailOutlined class="site-form-item-icon"/>
-                        </template>
-                      </a-input>
-                    </a-form-item>
-                    <a-form-item
-                        :rules="[{ required: true, message: '请输入新密码' }]"
-                        label="新密码"
-                        name="newPassword"
-                    >
-                      <a-input-password v-model:value="data.newPassword">
-                        <template #prefix>
-                          <SafetyOutlined class="site-form-item-icon"/>
-                        </template>
-                      </a-input-password>
-                    </a-form-item>
-                    <a-form-item
-                        :rules="[{ required: true, message: '请再输入一次' }]"
-                        label="新密码"
-                        name="newPassword"
-                    >
-                      <a-input-password v-model:value="data.newPasswordConfirm">
-                        <template #prefix>
-                          <KeyOutlined class="site-form-item-icon"/>
-                        </template>
-                      </a-input-password>
-                    </a-form-item>
-                    <div class="text-center">
-                      <a-button :disabled="false" class="bg-aspargus" html-type="submit" type="primary"
-                                @click="ConfirmChange">
-                        <div class="mx-10">确认修改</div>
-                      </a-button>
-                    </div>
-                  </a-form>
-                </div>
+                    <a-input-password v-model:value="data.currentPassword">
+                      <template #prefix>
+                        <KeyOutlined class="site-form-item-icon"/>
+                      </template>
+                    </a-input-password>
+                  </a-form-item>
+                  <a-form-item
+                      :rules="[{ required: true, message: '请输入新密码' }]"
+                      label="新密码"
+                      name="newPassword"
+                      extra="必须包含大小写字母和数字的组合，可以使用特殊字符，长度在6-30之间"
+                  >
+                    <a-input-password v-model:value="data.newPassword">
+                      <template #prefix>
+                        <KeyOutlined class="site-form-item-icon"/>
+                      </template>
+                    </a-input-password>
+                  </a-form-item>
+                  <a-form-item
+                      :rules="[{ required: true, message: '请再输入一次' }]"
+                      label="请再输一次"
+                      name="newPassword"
+                      extra="必须包含大小写字母和数字的组合，可以使用特殊字符，长度在6-30之间"
+                  >
+                    <a-input-password v-model:value="data.newPasswordConfirm">
+                      <template #prefix>
+                        <KeyOutlined class="site-form-item-icon"/>
+                      </template>
+                    </a-input-password>
+                  </a-form-item>
+                  <div class="text-center">
+                    <a-button :disabled="false" class="bg-aspargus" html-type="submit" type="primary"
+                              @click="ConfirmChange">
+                      <div class="mx-10">确认修改</div>
+                    </a-button>
+                  </div>
+                </a-form>
               </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
@@ -61,23 +66,28 @@
 </template>
 
 <script setup>
-import {KeyOutlined, MailOutlined, SafetyOutlined, ToolTwoTone} from "@ant-design/icons-vue";
-import { reactive } from 'vue';
+import {KeyOutlined, ToolTwoTone} from "@ant-design/icons-vue";
+import {reactive} from 'vue';
 import request from "@/assets/js/Request.js";
 import {message} from "ant-design-vue";
-import requests from "@/assets/js/Request.js";
 
 const data = reactive({
-  currentPassword:'',
-  newPassword:'',
-  newPasswordConfirm:''
+  currentPassword: '',
+  newPassword: '',
+  newPasswordConfirm: ''
 })
 
-function ConfirmChange(){
-    request.Change(data).then((res) => {
-      switch (data.currentPassword){
-
-      }
-    })
+function ConfirmChange() {
+  request.Change(data).then((res) => {
+    message.success(res.data.message);
+  }).catch((err) => {
+    switch (err.response.data.output) {
+      case "RequestBodyError":
+        message.warn(err.response.data.data[0]);
+        break;
+      default:
+        message.warn(err.response.data.message)
+    }
+  })
 }
 </script>
